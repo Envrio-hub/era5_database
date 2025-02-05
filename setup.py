0df9548457d88e51638cff6df846a8a20cf86e45
itsakmak <xylopodaros@yahoo.gr>
2025-02-05 10:17:32 +0200

    testing requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     python_requires='>=3.12',
     install_requires=[  
         'sqlalchemy>=2.0.37',
-        'aws_utils @ {ref = "1.1.0", git = "https://github.com/Envrio-hub/aws_utils.git"}',
         'mysql-connector-python>=9.2.0',
         'pydantic>=2.10.6',
         'influxdb-client>=1.48.0'
